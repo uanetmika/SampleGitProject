02381ba5c19ef2c0c73ccc3b2a621564e6445896
uanetmika <78543199+uanetmika@users.noreply.github.com>
2021-03-28 03:01:18 +0700

    Add files via upload

---
+++ exam2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("pls input number a : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("pls input number b : ");
+            int b = int.Parse(Console.ReadLine());
+
+            int x = a;
+            int y = b;
+
+            do
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            while (x != y);
+            {
+                Console.Write(x);
+            }
+
+            Console.ReadLine();
+
+        }
+
+    } 
+
+    
+}
