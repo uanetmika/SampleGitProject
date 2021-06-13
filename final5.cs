ddedd1f982f4f56f8f62aa72737ccb6cd67e7e71
uanetmika <78543199+uanetmika@users.noreply.github.com>
2021-06-13 17:26:50 +0700

    Add files via upload

---
+++ final5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number item : ");
+            int num = int.Parse(Console.ReadLine());
+
+            while (num <= 0)
+            {
+                Console.Write("number item : ");
+                num = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Item : ");
+            string item = Console.ReadLine();
+            Console.Write("Type : ");
+            string type = Console.ReadLine();
+            
+        }
+    }
+}
