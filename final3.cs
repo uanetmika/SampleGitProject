fb43211908bfe139acf9ee72523fa6529746b29f
uanetmika <78543199+uanetmika@users.noreply.github.com>
2021-06-13 17:19:34 +0700

    Add files via upload

---
+++ final3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("number : ");
+            string num = Console.ReadLine();
+            
+
+
+            do
+            {
+
+                Console.Write("number : ");
+                num = Console.ReadLine();
+                
+
+
+            } while (num != "End");
+
+            
+        }
+    }
+}
