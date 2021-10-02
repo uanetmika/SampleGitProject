d75f95d434ef709ea98b6c62896a522ff80493df
uanetmika <78543199+uanetmika@users.noreply.github.com>
2021-10-03 03:28:33 +0700

    Add files via upload

---
+++ TT.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ควยๆๆๆ ลอกอยู่นั่นแหละไอหน้าสัส หัดทำเองบ้างอีเหี้ย");
+        }
+    }
+}
